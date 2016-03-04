66243ea08f3e831476a11a4db346cb46bfe6c5b5
Nisay <zozuliy@gmail.com>
2016-03-04 19:58:04 +0200

    New changes
    Add new folders and models

---
+++ PlaeyrGenerator.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class PlaeyrGenerator : MonoBehaviour {
+    public GameObject charecter;
+    public Transform startPosition;
+
+    // Dictionary<name of part, mesh>
+    private Dictionary<string, Mesh> currentParts = new Dictionary<string, Mesh>();
+    //Dictionry<name of part, name of mesh>;
+    private Dictionary<string, string> partsNames = new Dictionary<string, string>();
+
+
+    void ChangePart(string part, string meshName)
+    {
+
+    }
+
+    void AddCharacter(Dictionary<string, string> newParts)
+    {
+
+        foreach (KeyValuePair<string, string> entry in newParts)
+        {
+
+            ChangePart(entry.Key, entry.Value);
+        }
+
+    }
+
+    //Use this for initialization
+    void Start()
+    {
+        Instantiate(charecter);
+        AddCharacter(partsNames);
+    }
+
+    }
