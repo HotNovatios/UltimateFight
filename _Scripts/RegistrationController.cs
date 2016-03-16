ad6a6e4e99d31d0c7dfaf2688b72d58049e7d259
Nisaiy <zozuliy@gmail.com>
2016-03-16 14:31:10 +0200

    Changes in Controllers

--- _Scripts/RegistrationController.cs
+++ _Scripts/RegistrationController.cs
@@ -21,6 +21,7 @@ public class RegistrationController : MonoBehaviour {
         GameObject go = GameObject.Find(where);
         GameObject errorField = Instantiate(FieldPrefab, new Vector3(go.transform.position.x-160, go.transform.position.y + 30, go.transform.position.z) , Quaternion.identity) as GameObject;
         errorField.transform.SetParent(go.transform);
+        errorField.GetComponent<InputField>().text = wich;
 
         Destroy(errorField, 5f);
 
@@ -29,26 +30,50 @@ public class RegistrationController : MonoBehaviour {
 
     bool isFieldsEqual(string main, string equal)
     {
-        return System.String.Equals(main, equal);
+        if (System.String.Equals(main, equal))
+        {
+            return true;
+        }
+
+        //errorMassage(main, "Do not matches with conffirm field");
+        return false;
     }
 
     bool validateEmaile(string email)
     {
-        return Regex.IsMatch(email,
+        if (Regex.IsMatch(email,
               @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
-              @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+              @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$"))
+        {
+            return true;
+        }
+        errorMassage("E-mail", "Wrong E-mail");
+        return false;
     }
 
     bool validatePassword(string password)
     {
-        return Regex.IsMatch(password, @"^(?=.*[a - z])(?=.*[A - Z])(?=.*\d).{ 8,15}$");
+        if (Regex.IsMatch(password, @"^(?=.*[a - z])(?=.*[A - Z])(?=.*\d).{ 8,15}$"))
+        {
+            return true;
+        }
+
+        errorMassage("Password", "Wrong Password");
+
+        return false;
     }
 
 
 
     bool validateLogin(string Login)
     {
-        return Regex.IsMatch(Login, @"^[A-Za-z0-9]{3-20}$");
+        if (Regex.IsMatch(Login, @"^[A-Za-z0-9]{3-20}$"))
+        {
+            return true;
+        }
+
+        errorMassage("Login", "Wrong Login");
+        return  false;
     }
 
 
@@ -79,9 +104,9 @@ public class RegistrationController : MonoBehaviour {
 
     public void Submit()
     {
-        errorMassage("Login", " ");
-        errorMassage("Password", " ");
-        errorMassage("E-mail", " ");
+        //errorMassage("Login", "Wrong Loin");
+        //errorMassage("password", "Wrong Password");
+        //errorMassage("email", "Wrong E-mail");
         if (chekInfo())
         {
             gameObject.SetActive(false);
