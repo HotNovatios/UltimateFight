83735c6f80db85873a524903a84907f079336eb8
Nisaiy <zozuliy@gmail.com>
2016-03-16 13:50:20 +0200

    Registration Controller

--- _Scripts/RegistrationController.cs
+++ _Scripts/RegistrationController.cs
@@ -1,42 +1,92 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System;
+using System.Text.RegularExpressions;
+using UnityEngine.SceneManagement;
 
 public class RegistrationController : MonoBehaviour {
 
     public InputField login;
     public InputField password;
 	public InputField confirmPassword;
-    public InputField mail;
+    public InputField Email;
+    public InputField confirmEmail;
 
-	public GameObject characterEditor;
-	public GameObject avatarsImage;
+    public GameObject FieldPrefab;
 
-    void Awake()
+
+    void errorMassage(string where, string wich)
     {
-        login.text = "LOG IN";
-        password.text = "PASSWORD";
-		confirmPassword.text = "CONFIRM PASSWORD";
-        mail.text = "E-MAIL";
+        GameObject go = GameObject.Find(where);
+        GameObject errorField = Instantiate(FieldPrefab, new Vector3(go.transform.position.x-160, go.transform.position.y + 30, go.transform.position.z) , Quaternion.identity) as GameObject;
+        errorField.transform.SetParent(go.transform);
 
-		characterEditor.SetActive (false);
+        Destroy(errorField, 5f);
+
     }
+
+
+    bool isFieldsEqual(string main, string equal)
+    {
+        return System.String.Equals(main, equal);
+    }
+
+    bool validateEmaile(string email)
+    {
+        return Regex.IsMatch(email,
+              @"^(?("")("".+?""@)|(([0-9a-zA-Z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-zA-Z])@))" +
+              @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,6}))$");
+    }
+
+    bool validatePassword(string password)
+    {
+        return Regex.IsMatch(password, @"^(?=.*[a - z])(?=.*[A - Z])(?=.*\d).{ 8,15}$");
+    }
+
+
+
+    bool validateLogin(string Login)
+    {
+        return Regex.IsMatch(Login, @"^[A-Za-z0-9]{3-20}$");
+    }
+
+
+
+
+    bool chekInfo()
+    {
+        bool check = true;
+
+        if (!validateLogin(login.text))
+        {
+            check = false;
+        }
+
+
+        if (!validateEmaile(Email.text) || !isFieldsEqual(Email.text, confirmEmail.text))
+        {
+            check = false;
+        }
+
+        if (!validatePassword(password.text) || !isFieldsEqual(password.text, confirmPassword.text))
+        {
+            check = false;
+        }
+        return check;
+    }
+
 
     public void Submit()
     {
-        // Get login and password
-        if (login.text == "" || password.text == "" || mail.text == "")
+        errorMassage("Login", " ");
+        errorMassage("Password", " ");
+        errorMassage("E-mail", " ");
+        if (chekInfo())
         {
-            Debug.Log("Fill the input fields");
-        }
-		else if(password.text != confirmPassword.text){
-			Debug.Log("Passwords do NOT match");
-		}
-		else {
-            Debug.Log("Success");
-			gameObject.SetActive(false);
-			characterEditor.SetActive(true);
-			avatarsImage.SetActive (false);
-        }
+            gameObject.SetActive(false);
+            SceneManager.LoadScene(1);
+        } 
+
     }
 }
