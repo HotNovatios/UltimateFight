f78abf28acc231cc96882d0bdc2e5e37500d559c
Nisaiy <zozuliy@gmail.com>
2016-04-03 21:59:52 +0300

    New cloakroom interface

---
+++ _Scripts/LockerInter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockerInter : MonoBehaviour
+{
+    public GameObject go;
+    public Renderer rend;
+    private Color curent;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        curent = rend.material.color;
+    }
+    void OnMouseEnter()
+    {
+        go.GetComponent<Animator>().Play("LockerDoorOpen", 0);
+        rend.material.color = Color.red;
+    }
+    void OnMouseOver()
+    {
+        rend.material.color -= new Color(0.1F, 0, 0) * Time.deltaTime;
+    }
+    void OnMouseExit()
+    {
+        go.GetComponent<Animator>().Play("LockerDoorclose", 0);
+        rend.material.color = curent;
+    }
+}
